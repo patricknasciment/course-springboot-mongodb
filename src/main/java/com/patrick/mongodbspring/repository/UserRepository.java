7a27d926a95e9790a308e8d293d83f961b09bf4f
patricknasciment <patrick_evan@outlook.com>
2022-07-15 22:51:54 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/patrick/mongodbspring/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.patrick.mongodbspring.repository;
+
+import com.patrick.mongodbspring.domain.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+}
