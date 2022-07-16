4d8c4af492fea9ecc68680e738bfc044a773d70e
patricknasciment <patrick_evan@outlook.com>
2022-07-15 23:30:03 -0300

    Obtendo um usuário por id

--- src/main/java/com/patrick/mongodbspring/resources/UserResources.java
+++ src/main/java/com/patrick/mongodbspring/resources/UserResources.java
@@ -5,10 +5,7 @@ import com.patrick.mongodbspring.dto.UserDTO;
 import com.patrick.mongodbspring.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -29,4 +26,9 @@ public class UserResources {
         return ResponseEntity.ok().body(listDto);
     }
 
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
+    }
 }
---
+++ src/main/java/com/patrick/mongodbspring/resources/exception/ResourceExceptionHandler.java
@@ -0,0 +1,21 @@
+package com.patrick.mongodbspring.resources.exception;
+
+import com.patrick.mongodbspring.services.exception.ObjectNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler{
+
+    @ExceptionHandler(ObjectNotFoundException.class)
+    public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request){
+
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(System.currentTimeMillis(), status.value(), "Não encontrado", e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+}
