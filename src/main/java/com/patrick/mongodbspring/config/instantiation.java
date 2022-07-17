51915ccd78c2d614e78d72731b92d77ad9ff5f7e
patricknasciment <patrick_evan@outlook.com>
2022-07-16 21:22:14 -0300

    Referenciando os posts do usuário

--- src/main/java/com/patrick/mongodbspring/config/instantiation.java
+++ src/main/java/com/patrick/mongodbspring/config/instantiation.java
@@ -39,5 +39,8 @@ public class instantiation implements CommandLineRunner {
         Post post2 = new Post(null, sdf.parse("17/07/2022"), "Mudança pronta", "Acordei feliz hoje!", new AuthorDTO(maria));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        userRepository.save(maria);
     }
 }
